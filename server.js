5c823882f1d4d6e8ecddae47b32559bf2d2dc7ac
Maurice Consultant <mainna62@outlook.com>
2025-09-07 14:40:26 +0300

    Add server.js to bind to 0.0.0.0:PORT

---
+++ server.js
@@ -0,0 +1,18 @@
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
+
+const port = process.env.PORT || 3000;
+const hostname = "0.0.0.0";
+const app = next({ dev: false, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port, hostname, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://${hostname}:${port}`);
+  });
+});
