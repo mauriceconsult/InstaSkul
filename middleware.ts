6b610807c0a2d3055fbbf32aba48e784e26a2f61
Maurice Consultant <mainna62@outlook.com>
2025-10-20 10:39:06 +0300

    Revert "Final commit 1"
    This reverts commit 7a54513940ce3b1276df88ba179e23d56f20d25e.

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs/server";
 
 export default authMiddleware({
+  // no public routes; all require Clerk auth
   publicRoutes: ["/api/health", "/api/public/hello"],
 });
 
