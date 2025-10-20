7a54513940ce3b1276df88ba179e23d56f20d25e
Maurice Consultant <mainna62@outlook.com>
2025-10-20 10:33:46 +0300

    Final commit 1

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
 import { authMiddleware } from "@clerk/nextjs/server";
 
 export default authMiddleware({
-  // no public routes; all require Clerk auth
   publicRoutes: ["/api/health", "/api/public/hello"],
 });
 
